83c0d177c3557bba28e2903bceed57cedce6d993
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-02-13 12:12:19 +1000

    removed Icon integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
   site: "https://fm-recipe-finder-website.netlify.app/",
   image: {
     responsiveStyles: true,
